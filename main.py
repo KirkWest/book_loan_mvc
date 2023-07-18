e3f38a520feaff8034c373153133d97e3f63fa6c
KirkWest <kirkwestsooby@gmail.com>
2023-07-18 16:47:52 +1000

    Created initial main, init, environments, and installed libraries

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import os
+from init import db, ma, bcrypt, jwt # imports instances
+
+# define app
+def create_app():
+    app = Flask(__name__) # create flask app
+    
+    # data bases URI and JWT secret key      dbms       driver     user      pwd     uri    port  db_name
+    app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("DATABASE_URL")
+    app.config["JWT_SECRET_KEY"]=os.environ.get("JWT_SECRET_KEY")
+
+    # initialise with application
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    # return that app from the create_app function
+    return app
