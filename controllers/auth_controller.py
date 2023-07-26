73715c86dc87c2b90673a5cd4aa5eaa53b46ded9
KirkWest <kirkwestsooby@gmail.com>
2023-07-26 15:41:22 +1000

    added in a get all users in auth_controller

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from init import db, bcrypt
 from models.user import User, user_schema, users_schema
 from flask_jwt_extended import create_access_token
@@ -22,7 +22,7 @@ def auth_register():
         user.name = body_data.get('name')
         user.email = body_data.get('email')
         user.address = body_data.get('address')
-        if body_data.get('password'):
+        if body_data.get('password'): # generates a hashed password
             user.password = bcrypt.generate_password_hash(body_data.get('password')).decode('utf-8')
 
         db.session.add(user)
@@ -37,11 +37,20 @@ def auth_register():
 
 @auth_bp.route('/login', methods=['POST']) # login route for users with checks for existing user and correct password
 def auth_login():
-    body_data = request.get_json() # checks db for user
+    body_data = request.get_json() # gets JSOn data from body
     stmt = db.select(User).filter_by(email=body_data.get('email'))
     user = db.session.scalar(stmt)
-    if user and bcrypt.check_password_hash(user.password, body_data.get('password')): # checking if password is valid
+
+    # if user exists it checks the password matches what is stored and returns the users email, token, and admin status
+    if user and bcrypt.check_password_hash(user.password, body_data.get('password')):
         token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1))
         return{'email': user.email, 'token': token, 'is_admin': user.is_admin}
     else:
+        # error message if user doesn't exist
         return {'error': 'Invalid email or password'}, 401
+    
+# gets all users
+@auth_bp.route('/get_all_users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    return users_schema.jsonify(users), 200
